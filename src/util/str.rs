4aa95863fb26959a4cd669d78a1d12ba260d8f28
wangjun <ZiCheng354892>
2024-04-25 22:56:13 +0800

    i

--- src/util/str.rs
+++ src/util/str.rs
@@ -1,4 +1,5 @@
 use std::string::FromUtf16Error;
+use bytes::Buf;
 
 pub fn octet_string(s: String, fixed_length: usize) -> String {
     let length = s.len();
@@ -23,6 +24,8 @@ pub fn oct_string(v: Vec<u8>) -> String {
 
 // 将假定为大端序UTF-16（即UCS-2）编码的字节切片转换为UTF-8字符串
 pub fn ucs2_to_utf8(ucs2_bytes: &[u8]) -> Result<String, FromUtf16Error> {
+
+
     // 确保字节数组的长度是 2 的倍数，因为每个 UCS-2 字符是 2 个字节
     if ucs2_bytes.len() % 2 != 0 {
         panic!("UCS-2 byte array length must be even");
@@ -31,7 +34,7 @@ pub fn ucs2_to_utf8(ucs2_bytes: &[u8]) -> Result<String, FromUtf16Error> {
     // 将字节数组解码为 u16 切片
     let utf16_chars: Vec<u16> = ucs2_bytes
         .chunks_exact(2)
-        .map(|chunk| u16::from_le_bytes(chunk.try_into().unwrap()))
+        .map(|chunk| u16::from_be_bytes(chunk.try_into().unwrap()))
         .collect();
 
     // 将 UTF-16 切片转换为 UTF-8 字符串
