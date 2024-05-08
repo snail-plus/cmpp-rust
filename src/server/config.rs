0f55e693a75f2985f6ce14be1053514bab416109
snail <9914016+snail-plus@users.noreply.github.com>
2024-05-08 10:06:44 +0800

    Update config.rs
    8

--- src/server/config.rs
+++ src/server/config.rs
@@ -1,6 +1,5 @@
 
 pub const DEFAULT_LISTENING_ADDR: &'static str = "0.0.0.0:8888";
-pub const TARGET_ADDR: &'static str = "39.108.113.237:6379";
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Config {
