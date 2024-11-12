fe866fcd6c795836ac18ebd3aae7974d9a47fe9e
120985 <308306362@qq.com>
2024-11-12 13:36:18 +0800

    fix: 包读取不全问题

--- src/server/conn.rs
+++ src/server/conn.rs
@@ -9,8 +9,8 @@ use crate::server::cmd::Command;
 use crate::server::handler::MsgInHandler;
 use crate::server::Result;
 
-pub trait  AuthHandler : Send + Sync{
-    fn  auth(&self, req: &cmd::connect::CmppConnReqPkt, res: &mut cmd::connect::Cmpp3ConnRspPkt) -> bool;
+pub trait AuthHandler: Send + Sync {
+    fn auth(&self, req: &cmd::connect::CmppConnReqPkt, res: &mut cmd::connect::Cmpp3ConnRspPkt) -> bool;
 }
 
 pub struct DefaultAuthHandler {}
@@ -25,18 +25,18 @@ impl AuthHandler for DefaultAuthHandler {
 
 pub struct Conn {
     buf: BytesMut,
-    auth_handler:  Box<dyn AuthHandler>,
+    auth_handler: Box<dyn AuthHandler>,
 }
 
 impl Conn {
     pub fn new() -> Conn {
         let buf = BytesMut::with_capacity(2048);
-        Conn {buf, auth_handler: Box::new(DefaultAuthHandler {})}
+        Conn { buf, auth_handler: Box::new(DefaultAuthHandler {}) }
     }
 
     pub async fn run(&mut self, stream: TcpStream) -> Result<()> {
         let (mut reader, mut writer) = io::split(stream);
-        
+
         let (tx_in, rx_in) = tokio::sync::mpsc::channel(1024);
         let (tx_out, mut rx_out) = tokio::sync::mpsc::channel(1024);
 
@@ -54,41 +54,38 @@ impl Conn {
         });
 
         loop {
-             match self.read_frame(&mut reader).await? {
-                 Some(req) => {
-                     match req {
-                         Command::Connect(ref req_c) => {
-                             let mut res = req.apply()?;
-                             match res {
-                                 Command::ConnectRsp(ref mut res_c) => {
-                                     let auth_result = self.auth_handler.auth(&req_c, res_c);
-                                     tx_out.clone().send(res).await.unwrap();
-                                     if !auth_result {
-                                         return Err("认证失败".into())
-                                     }
-                                 }
-                                 _ => {}
-                             }
-                         }
-                         _ => {
-                             let sender = tx_in.clone();
-                             sender.send(req).await?;
-                         }
-                     }
-                 }
-                 None => {}
-             }
+            if let Some(req) = self.read_frame(&mut reader).await? {
+                match req {
+                    Command::Connect(ref req_c) => {
+                        let mut res = req.apply()?;
+                        match res {
+                            Command::ConnectRsp(ref mut res_c) => {
+                                let auth_result = self.auth_handler.auth(&req_c, res_c);
+                                tx_out.clone().send(res).await?;
+                                if !auth_result {
+                                    return Err("认证失败".into());
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {
+                        let sender = tx_in.clone();
+                        sender.send(req).await?;
+                    }
+                }
+            }
         }
     }
+
 
     async fn read_frame(&mut self, reader: &mut ReadHalf<TcpStream>) -> Result<Option<Command>> {
         let mut decoder = CmppDecoder::default();
 
         loop {
-
             if let Some(mut frame) = decoder.decode(&mut self.buf)? {
                 let req = Command::parse_frame(frame.command_id, frame.seq_id, &mut frame.body_data)?;
-                return Ok(Some(req))
+                return Ok(Some(req));
             }
 
             if 0 == reader.read_buf(&mut self.buf).await? {
@@ -97,7 +94,7 @@ impl Conn {
                 } else {
                     let s = "connection reset by peer".into();
                     Err(s)
-                }
+                };
             }
         }
     }
